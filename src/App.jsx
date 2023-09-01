6e19dd1261d4ef2d0c86bfdb26114ce65f8b9be0
paul linden <paul.linden@gritacademy.se>
2023-09-01 10:10:57 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createRoot } from 'react-dom/client';
+
+import './index.css';
+
+import Header from './modules/Header';
+import Main from './modules/Main';
+
+function App() {
+
+    //const [status, setStatus] = useState('');
+
+    return (
+        <div className="dark:bg-gray-700 h-fit min-h-screen  w-screen font-serif">
+
+            <Header/>
+            
+            <Main/>
+            
+        </div>
+    );
+}
+
+const root = createRoot(document.querySelector("#root"));
+root.render(<App />);
